20051f60f7c4264fa5a0507fa0388dee9b260920
Shayenka Alvarado <shayenkalvarado@gmail.com>
2023-01-17 02:01:01 -0600

    "style: Ajustar resoluciones"

--- script.js
+++ script.js
@@ -2,25 +2,35 @@ let buttonEdit = document.querySelector(".profile__edit");
 let buttonClose = document.querySelector(".popup__close-icon");
 let formElement = document.querySelector(".popup_opened");
 
-let inputName = document.querySelector("#input-name");
-let inputAbout = document.querySelector("#input-about");
+let inputName = document.querySelector("#name");
+let inputAbout = document.querySelector("#about");
 let profileName = document.querySelector(".profile__name");
 let profileOccupation = document.querySelector(".profile__occupation");
 let buttonSubmit = document.querySelector(".popup__button");
 
-function openPopup () {
+function openClosePopup() {
     formElement.classList.toggle("popup_opened");
     
 }
 
-buttonEdit.addEventListener ("click", openPopup);
-buttonClose.addEventListener ("click", openPopup);
+buttonEdit.addEventListener ("click", openClosePopup);
+buttonClose.addEventListener ("click", openClosePopup);
+
+function setProfileValues() {
+    profileName.textContent = inputName.value;
+    profileOccupation.textContent = inputAbout.value;
+}
+
+function resetPopup() {
+    inputName.value="";
+    inputAbout.value="";
+}
 
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
-    profileName.textContent = inputName.value;
-    profileOccupation.textContent = inputAbout.value;
-
-    }
+    setProfileValues();
+    openClosePopup();
+    resetPopup();
+}
 
 buttonSubmit.addEventListener ("click", handleProfileFormSubmit);
